4515706d095bae6589c763d4782dad4970a63cc8
sidesai2001 <sidesai2001@gmail.com>
2022-02-21 14:10:05 +0530

    changes

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white">BloodBank & Donor Management System 2021-22</p>
+        </div>
+        <!-- /.container -->
+    </footer>
---
+++ index.php
@@ -0,0 +1,134 @@
+<?php
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>BloodBank & Donor Management System</title>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <style>
+    .navbar-toggler {
+        z-index: 1;
+    }
+    
+    @media (max-width: 576px) {
+        nav > .container {
+            width: 100%;
+        }
+    }
+    .carousel-item.active,
+    .carousel-item-next,
+    .carousel-item-prev {
+        display: block;
+    }
+    </style>
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+<?php include('includes/header.php');?>
+<?php include('includes/slider.php');?>
+   
+    <!-- Page Content -->
+    <div class="container">
+
+        <h1 class="my-4">Welcome to BloodBank & Donor Management System</h1>
+
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header">The need for blood</h4>
+                   
+                        <p class="card-text" style="padding-left:2%">Every year our nation requires about 5 Crore units of blood, out of which only a meager 2.5 Crore units of blood are available.
+                        <br>More than 38,000 blood donations are needed every day.
+                        <br>A single car accident victim can require as many as 100 units of blood </p>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header">Blood Tips</h4>
+                   
+                        <p class="card-text" style="padding-left:2%"><ul><li>Drink an extra 16 oz. of water (or other nonalcoholic drink) before your appointment.</li>
+                            <li>Eat a healthy meal, avoiding fatty foods like hamburgers, fries or ice cream.</li>
+                            <li>Let us know if you have a preferred arm or particular vein that has been used successfully in the past to draw blood.</li>
+                            </ul></p>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header">Who you could Help</h4>
+                   
+                        <p class="card-text" style="padding-left:2%">Every day, blood donors help patients of all ages: accident and burn victims, heart surgery and organ transplant patients, and those battling cancer.</p>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+  
+
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>BLOOD GROUPS</h2>
+          <p>  Blood group of any human being will mainly fall in any one of the following groups.</p>
+                <ul>
+                
+                
+<li>A positive or A negative</li>
+<li>B positive or B negative</li>
+<li>O positive or O negative</li>
+<li>AB positive or AB negative.</li>
+                </ul>
+                <p>A healthy diet helps ensure a successful blood donation, and also makes you feel better! Check out the following recommended foods to eat prior to your donation.</p>
+            </div>
+            <div class="col-lg-6">
+                <img class="img-fluid rounded" src="images/blood-donor.jpg" alt="">
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Call to Action Section -->
+        <div class="row mb-4">
+            <div class="col-md-8">
+            <h4>UNIVERSAL DONORS AND RECIPIENTS</h4>
+                <p>
+The most common blood type is O, followed by type A.
+
+Type O individuals are often called "universal donors" since their blood can be transfused into persons with any blood type. Those with type AB blood are called "universal recipients" because they can receive blood of any type.</p>
+            </div>
+        <div class="row mb-4">
+            
+            <div class="col-md-4">
+                <a class="btn btn-lg btn-secondary btn-block" href="become-donar.php">Become a Donar</a>
+            </div>
+        </div>
+
+   
+    <!-- /.container -->
+
+    <!-- Footer -->
+  <?php include('includes/footer.php');?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/tether/tether.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
